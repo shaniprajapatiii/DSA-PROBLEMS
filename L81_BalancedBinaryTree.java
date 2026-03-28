fe533c42cd5f82d4c52f9b27997537dd1d3dff7b
Shani Prajapati <shaniprajapati630@gmail.com>
2026-03-28 15:12:18 +0530

    balanced and symmetric tree algorithms and taking input using bfs

--- L81_BalancedBinaryTree.java
+++ L81_BalancedBinaryTree.java
@@ -1,3 +1,104 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class L81_BalancedBinaryTree {
-   
+   static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode(int val) {
+         this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
+
+   public boolean isBalanced(TreeNode root) {
+      return heightOrUnbalanced(root) != -1;
+   }
+
+   private int heightOrUnbalanced(TreeNode node) {
+      if (node == null) {
+         return 0;
+      }
+
+      int leftHeight = heightOrUnbalanced(node.left);
+      if (leftHeight == -1) {
+         return -1;
+      }
+
+      int rightHeight = heightOrUnbalanced(node.right);
+      if (rightHeight == -1) {
+         return -1;
+      }
+
+      if (Math.abs(leftHeight - rightHeight) > 1) {
+         return -1;
+      }
+
+      return Math.max(leftHeight, rightHeight) + 1;
+   }
+
+   private static TreeNode buildTreeFromLevelOrder(Integer[] values) {
+      if (values == null || values.length == 0 || values[0] == null) {
+         return null;
+      }
+
+      TreeNode root = new TreeNode(values[0]);
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+
+      int i = 1;
+      while (!queue.isEmpty() && i < values.length) {
+         TreeNode current = queue.poll();
+
+         if (i < values.length && values[i] != null) {
+            current.left = new TreeNode(values[i]);
+            queue.offer(current.left);
+         }
+         i++;
+
+         if (i < values.length && values[i] != null) {
+            current.right = new TreeNode(values[i]);
+            queue.offer(current.right);
+         }
+         i++;
+      }
+
+      return root;
+   }
+
+   public static void main(String[] args) {
+      L81_BalancedBinaryTree solver = new L81_BalancedBinaryTree();
+
+      // Level-order input: null means the child at that position does not exist.
+      // balancedInput = [3,9,20,null,null,15,7]
+      //        3
+      //      /   \
+      //     9     20
+      //          /  \
+      //         15   7
+      //
+      // unbalancedInput = [1,2,null,3,null,4,null]
+      //     1
+      //    /
+      //   2
+      //  /
+      // 3
+      // /
+      //4
+      Integer[] balancedInput = {3, 9, 20, null, null, 15, 7};
+      Integer[] unbalancedInput = {1, 2, null, 3, null, 4, null};
+
+      TreeNode balanced = buildTreeFromLevelOrder(balancedInput);
+      TreeNode unbalanced = buildTreeFromLevelOrder(unbalancedInput);
+
+      System.out.println("Balanced tree: " + solver.isBalanced(balanced));
+      System.out.println("Unbalanced tree: " + solver.isBalanced(unbalanced));
+   }
 }
--- L82_SymmetricTree.java
+++ L82_SymmetricTree.java
@@ -1,3 +1,96 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class L82_SymmetricTree {
-   
+   static class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+
+      TreeNode(int val) {
+         this.val = val;
+      }
+
+      TreeNode(int val, TreeNode left, TreeNode right) {
+         this.val = val;
+         this.left = left;
+         this.right = right;
+      }
+   }
+
+   public boolean isSymmetric(TreeNode root) {
+      if (root == null) {
+         return true;
+      }
+      return isMirror(root.left, root.right);
+   }
+
+   private boolean isMirror(TreeNode left, TreeNode right) {
+      if (left == null && right == null) {
+         return true;
+      }
+      if (left == null || right == null) {
+         return false;
+      }
+      if (left.val != right.val) {
+         return false;
+      }
+      return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+   }
+
+   private static TreeNode buildTreeFromLevelOrder(Integer[] values) {
+      if (values == null || values.length == 0 || values[0] == null) {
+         return null;
+      }
+
+      TreeNode root = new TreeNode(values[0]);
+      Queue<TreeNode> queue = new LinkedList<>();
+      queue.offer(root);
+
+      int i = 1;
+      while (!queue.isEmpty() && i < values.length) {
+         TreeNode current = queue.poll();
+
+         if (i < values.length && values[i] != null) {
+            current.left = new TreeNode(values[i]);
+            queue.offer(current.left);
+         }
+         i++;
+
+         if (i < values.length && values[i] != null) {
+            current.right = new TreeNode(values[i]);
+            queue.offer(current.right);
+         }
+         i++;
+      }
+
+      return root;
+   }
+
+   public static void main(String[] args) {
+      L82_SymmetricTree solver = new L82_SymmetricTree();
+
+      // Level-order input: null means the child at that position does not exist.
+      // symmetricInput = [1,2,2,3,4,4,3]
+      //         1
+      //      /     \
+      //     2       2
+      //    / \     / \
+      //   3   4   4   3
+      //
+      // notSymmetricInput = [1,2,2,null,3,null,3]
+      //         1
+      //      /     \
+      //     2       2
+      //      \       \
+      //       3       3
+      Integer[] symmetricInput = {1, 2, 2, 3, 4, 4, 3};
+      Integer[] notSymmetricInput = {1, 2, 2, null, 3, null, 3};
+
+      TreeNode symmetric = buildTreeFromLevelOrder(symmetricInput);
+      TreeNode notSymmetric = buildTreeFromLevelOrder(notSymmetricInput);
+
+      System.out.println("Symmetric tree: " + solver.isSymmetric(symmetric));
+      System.out.println("Not symmetric tree: " + solver.isSymmetric(notSymmetric));
+   }
 }
